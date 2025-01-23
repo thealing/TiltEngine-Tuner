8c1fa3fd4ffd54b18507cdce6989d3832486d24c
thealing <159199097+thealing@users.noreply.github.com>
2025-01-23 21:43:10 +0100

    Changed Intrinsics To <bit> Methods

--- src/bitboards.hpp
+++ src/bitboards.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <stdint.h>
-#include <intrin.h>
+#include <cstdint>
+#include <bit>
 
 inline constexpr bool test_square(uint64_t bitboard, int square)
 {
@@ -20,7 +20,7 @@ inline constexpr void clear_square(uint64_t& bitboard, int square)
 
 inline int get_square(uint64_t bitboard)
 {
-	return (int)_tzcnt_u64(bitboard);
+	return std::countr_zero(bitboard);
 }
 
 inline int pop_square(uint64_t& bitboard)
@@ -32,12 +32,12 @@ inline int pop_square(uint64_t& bitboard)
 
 inline int count_squares(uint64_t bitboard)
 {
-	return (int)__popcnt64(bitboard);
+	return std::popcount(bitboard);
 }
 
 inline void flip_squares(uint64_t& bitboard)
 {
-	bitboard = _byteswap_uint64(bitboard);
+	bitboard = std::byteswap(bitboard);
 }
 
 inline uint64_t move_up(uint64_t bitboard)
